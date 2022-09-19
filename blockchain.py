ed8bf4358fb3dfe88839e678e4209c87c856f130
DanielKirshner <danielkirshner7878@gmail.com>
2022-09-19 21:13:23 +0300

    Created block class

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,14 @@
+class Block:
+    def __init__(self, index, time_span, data, previous_hash=''):
+        self.index = index
+        self.time_span = time_span
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+    
+
+    def calculate_hash():
+        pass
 
 
 def main():
