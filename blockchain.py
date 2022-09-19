2db18bdfb3fa2f47830b45efc34d55888d878fa4
DanielKirshner <danielkirshner7878@gmail.com>
2022-09-19 21:18:07 +0300

    Added block hash calculation function

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,7 @@
+import hashlib
+
 class Block:
-    def __init__(self, index, time_span, data, previous_hash=''):
+    def __init__(self, index: int, time_span: str, data: str, previous_hash=''):
         self.index = index
         self.time_span = time_span
         self.data = data
@@ -7,8 +9,9 @@ class Block:
         self.hash = self.calculate_hash()
     
 
-    def calculate_hash():
-        pass
+    def calculate_hash(self) -> str: 
+        all_block_data = str(self.index) + self.time_span + self.data + self.previous_hash
+        return hashlib.sha256(all_block_data.encode()).hexdigest()
 
 
 def main():
