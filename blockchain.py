c893bf37c82fc3b2b6619083d29b376135332acf
DanielKirshner <danielkirshner7878@gmail.com>
2022-09-22 21:15:17 +0300

    Created class 'BlockChain'

--- blockchain.py
+++ blockchain.py
@@ -13,15 +13,24 @@ class Block:
         all_block_data = str(self.index) + self.time_span + self.data + self.previous_hash
         return hashlib.sha256(all_block_data.encode()).hexdigest()
     
+
     def __str__(self):
         return 'block hash: ' + self.hash + '\n at index: ' + \
                 str(self.index) + ' created at: ' + self.time_span + \
                 ' data: ' + self.data + '\n previous hash: ' + self.previous_hash
 
 
+class BlockChain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+    
+
+    def create_genesis_block(self):
+        return Block(0, "01/01/2022", "Genesis Block", '0')
+
+
 def main():
-    b = Block(0, "01/01/2022", "abcdefghijklmnopqrstuvwxyz")
-    print(b)
+    pass
 
 
 if __name__ == "__main__":
