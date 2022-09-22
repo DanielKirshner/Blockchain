9feb2dd02d7ad1925101fc98d581288a6790f481
DanielKirshner <danielkirshner7878@gmail.com>
2022-09-22 21:10:53 +0300

    Added __str__ method for class 'block'

--- blockchain.py
+++ blockchain.py
@@ -12,10 +12,16 @@ class Block:
     def calculate_hash(self) -> str: 
         all_block_data = str(self.index) + self.time_span + self.data + self.previous_hash
         return hashlib.sha256(all_block_data.encode()).hexdigest()
+    
+    def __str__(self):
+        return 'block hash: ' + self.hash + '\n at index: ' + \
+                str(self.index) + ' created at: ' + self.time_span + \
+                ' data: ' + self.data + '\n previous hash: ' + self.previous_hash
 
 
 def main():
-    pass
+    b = Block(0, "01/01/2022", "abcdefghijklmnopqrstuvwxyz")
+    print(b)
 
 
 if __name__ == "__main__":
