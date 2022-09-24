5e534413e292fed604f89fa01936af6afcaf1808
DanielKirshner <danielkirshner7878@gmail.com>
2022-09-24 20:44:22 +0300

    Added chain validation check method

--- blockchain.py
+++ blockchain.py
@@ -38,6 +38,15 @@ class BlockChain:
         new_block.hash = new_block.calculate_hash()
         self.chain.append(new_block)
     
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+        if current_block.hash != current_block.calculate_hash() or current_block.previous_hash != previous_block.hash:
+            return False
+        return True
+    
     def __str__(self):
         chain = ''
         for block in self.chain:
